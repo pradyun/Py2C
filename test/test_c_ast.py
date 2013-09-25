40bff1bb130b3c257e037af1f319663350c67dd1
Pradyun <pradyunsg@gmail.com>
2013-09-25 18:46:19 +0530

    Modified the C AST generator to accept verbose option, could be helpful during command-line runs.
    Added indent function.
    Tests for fore mentioned.

--- test/test_c_ast.py
+++ test/test_c_ast.py
@@ -1,90 +1,122 @@
 #!/usr/bin/env python
 
+import sys
+import textwrap
 import StringIO
+
 import unittest
 
 import support
-
 with support.project_imports():
     import c_ast
+    c_ast.prepare()
 
-# ----------------------------------
+
 class ParserTestCase(unittest.TestCase):
     """Abstract TestCase: Tests for Parsing of structures"""
+
     def setUp(self):
         self.parser = c_ast.Parser()
 
-    def prepare(self, test_string):
+    def template(self, test_string, expected):
         self.test_string = test_string
         self.parser.prepare(StringIO.StringIO(test_string))
-        return self.parser.data
+        self.assertEqual(self.parser.data, expected)
+
 
 class CommentTestCase(ParserTestCase):
     """Tests for Parser parsing comments"""
+
     def test_comment_empty_line(self):
-        "Checks: Parser handles empty lines with comments correctly"
-        data = self.prepare("#test: a,string")
-        self.assertEqual(data, [])
+        "Checks if Parser handles empty lines with comments correctly"
+        self.template(
+            "#test: a,string",
+            []
+        )
 
     def test_comment_no_arg_no_space(self):
-        "Checks: Parser handles no argument Nodes with comments correctly"
-        data = self.prepare("foo:#test: a,string")
-        self.assertEqual(data, [['foo', []]])
+        "Checks if Parser handles no argument Nodes with comments correctly"
+        self.template(
+            "foo:#test: a,string",
+            [['foo', []]]
+        )
 
     def test_comment_no_arg_with_spaces(self):
-        "Checks: Parser handles no argument Nodes with comments correctly"
-        data = self.prepare("foo: #test: a,string")
-        self.assertEqual(data, [['foo', []]])
+        "Checks if Parser handles no argument Nodes with comments correctly"
+        self.template(
+            "foo: #test: a,string",
+            [['foo', []]]
+        )
 
     def test_comment_1arg_no_space(self):
-        "Checks: Parser handles 1 argument Nodes with comments correctly"
-        data = self.prepare("foo: bar#test: a,string")
-        self.assertEqual(data, [['foo', ['bar']]])
+        "Checks if Parser handles 1 argument Nodes with comments correctly"
+        self.template(
+            "foo: bar#test: a,string",
+            [['foo', ['bar']]]
+        )
 
     def test_comment_1arg_with_spaces(self):
-        "Checks: Parser handles 1 argument Nodes with comments correctly"
-        data = self.prepare("foo: bar #test: a,string")
-        self.assertEqual(data, [['foo', ['bar']]])
+        "Checks if Parser handles 1 argument Nodes with comments correctly"
+        self.template(
+            "foo: bar #test: a,string",
+            [['foo', ['bar']]]
+        )
 
     def test_comment_multi_arg_no_space(self):
-        "Checks: Parser handles multi argument Nodes with comments correctly"
-        data = self.prepare("foo: bar, baz#test: a,string")
-        self.assertEqual(data, [['foo', ['bar', 'baz']]])
+        "Checks if Parser handles multi argument Nodes with comments correctly"
+        self.template(
+            "foo: bar, baz#test: a,string",
+            [['foo', ['bar', 'baz']]]
+        )
 
     def test_comment_multi_arg_with_spaces(self):
-        "Checks: Parser handles multi argument Nodes with comments correctly"
-        data = self.prepare("foo: bar, baz #test: a,string")
-        self.assertEqual(data, [['foo', ['bar', 'baz']]])
+        "Checks if Parser handles multi argument Nodes with comments correctly"
+        self.template(
+            "foo: bar, baz #test: a,string",
+            [['foo', ['bar', 'baz']]]
+        )
+
 
 class PropertyTestCase(ParserTestCase):
     """Tests for Parser's parsing of Properties"""
+
     def test_property_empty(self):
-        data = self.prepare("foo:")
-        self.assertEqual(data, [['foo', []]])
+        self.template(
+            "foo:",
+            [['foo', []]]
+        )
 
     def test_property_1arg(self):
-        data = self.prepare("foo: bar")
-        self.assertEqual(data, [['foo', ['bar']]])
+        self.template(
+            "foo: bar",
+            [['foo', ['bar']]]
+        )
 
     def test_property_multi_arg(self):
-        data = self.prepare("foo: bar, baz")
-        self.assertEqual(data, [['foo', ['bar', 'baz']]])
+        self.template(
+            "foo: bar, baz",
+            [['foo', ['bar', 'baz']]]
+        )
 
     def test_property_1arg_default(self):
-        data = self.prepare("foo: bar=None")
-        self.assertEqual(data, [['foo', ['bar=None']]])
+        self.template(
+            "foo: bar=None",
+            [['foo', ['bar=None']]]
+        )
 
     def test_property_multi_arg_default(self):
-        data = self.prepare("foo: bar, baz=None")
-        self.assertEqual(data, [['foo', ['bar', 'baz=None']]])
+        self.template(
+            "foo: bar, baz=None",
+            [['foo', ['bar', 'baz=None']]]
+        )
 
     def test_invalid_object_to_prepare(self):
         self.assertRaises(TypeError, self.parser.prepare, 1)
 
-# There really is only one data structure which is generated,
-# so One test case is enough
+
 class GenerationTestCase(ParserTestCase):
     """Tests for Generation of classes from Data"""
+
     def get_result(self, data):
         self.parser.data = data
         di = {}
@@ -139,24 +171,71 @@ class GenerationTestCase(ParserTestCase):
     # Given anything with a __dict__ attribute
     def test_generator_not_dict(self):
         data = [['foo', []]]
+        # A class inheriting from object with no methods defined in it.
         cls = type('cls', (object,), {})
         obj = cls()
         self.parser.data = data
         self.parser.setup_module(obj)
         self.basic_checks(obj.__dict__, 'foo')
 
+    def test_verbose_output(self):
+        data = [['foo', ['bar']]]
+        self.parser.data = data
+        di = {}
+        str_out = StringIO.StringIO("") # a StringIO that replaces sys.stdout
+        # replace sys.stdout
+        old_sysout = sys.stdout
+        sys.stdout = str_out
+        # run verbosely
+        self.parser.setup_module(di, verbose=True)
+        # restore sys.stdout
+        sys.stdout = old_sysout
+        # get values printed during verbose run
+        result = str_out.getvalue()
+
+
+        self.assertEqual(result.strip(), textwrap.dedent("""
+            class foo(AST):
+                def __init__(self, bar):
+                    super(self.__class__, self).__init__()
+                    self._fields = ['bar']
+                    self.bar = bar
+            """).strip()
+        )
+
+    def test_indent1(self):
+        "Tests for indent function"
+        self.assertEqual(c_ast.indent("foo"), '    '+'foo')
+        self.assertEqual(c_ast.indent("foo\nbar"), '    foo\n    bar')
+        self.assertEqual(c_ast.indent("foo\n"), '    '+'foo')
+        self.assertEqual(c_ast.indent("\nfoo"), '    foo')
+
+
+
 # -----------------------------------
 class ASTTestCase(unittest.TestCase):
     """Tests for AST node"""
 
-    def test_equality_equal(self):
+    def test_equality_equal1(self):
         "Test for equality for really equal Nodes"
-        node1 = node2 = c_ast.AST()
+        def setup_node(node):
+            node._fields = map(chr, range(97, 103))
+            di = dict((chr(i), i) for i in range(97, 103))
+            node.__dict__.update(di)
+        node1 = c_ast.AST()
+        node2 = c_ast.AST()
         # add fields and attributes
-        node1._fields = map(chr, range(97, 103))
-        di = {chr(i): i for i in range(97, 103)}
-        node1.__dict__.update(di)
+        setup_node(node1)
+        setup_node(node2)
 
+        self.assertEqual(node1, node2)
+
+    def test_equality_equal2(self):
+        "Test for equality with node when has children nodes"
+        node1 = c_ast.Print(dest=c_ast.Name(id='foo'), values=[],
+                                    sep=' ', end='\n')
+        node2 = c_ast.Print(dest=c_ast.Name(id='foo'), values=[],
+                                    sep=' ', end='\n')
         self.assertEqual(node1, node2)
 
     def test_equality_with_extra_attrs(self):
@@ -166,10 +245,10 @@ class ASTTestCase(unittest.TestCase):
 
         # add fields and attributes
         node1._fields = map(chr, range(97, 103))
-        di = {chr(i): i for i in range(97, 103)}
+        di = dict((chr(i), i) for i in range(97, 103))
         node1.__dict__.update(di)
         node2._fields = node1._fields
-        di = {chr(i): i for i in range(97, 104)}
+        di = dict((chr(i), i) for i in range(97, 104))
         node2.__dict__.update(di)
 
         self.assertEqual(node1, node2)
@@ -181,7 +260,7 @@ class ASTTestCase(unittest.TestCase):
 
         # add fields and attributes
         node1._fields = map(chr, range(97, 103))
-        di = {chr(i): i for i in range(97, 103)}
+        di = dict((chr(i), i) for i in range(97, 103))
         node1.__dict__.update(di)
         node2.__dict__.update(di)
         node2._fields = node1._fields + ["i_will_make_the_two_equal"]
@@ -189,7 +268,7 @@ class ASTTestCase(unittest.TestCase):
         self.assertNotEqual(node1, node2)
 
     def test_equality_diff_type(self):
-        "Test for equality on the basis of type"
+        "Test for inequality on the basis of type"
         node1 = c_ast.AST()
 
         self.assertNotEqual(node1, "I'm not equal to a Node")
@@ -201,10 +280,10 @@ class ASTTestCase(unittest.TestCase):
 
         # add fields and attributes
         node1._fields = map(chr, range(97, 103))
-        di = {chr(i): i for i in range(97, 103)}
+        di = dict((chr(i), i) for i in range(97, 103))
         node1.__dict__.update(di)
         node2._fields = node1._fields
-        di = {chr(i): i for i in range(98, 104)}
+        di = dict((chr(i), i) for i in range(98, 104))
         node2.__dict__.update(di)
 
         self.assertNotEqual(node1, node2)
