33ba6ed74e702eda5e102deed236142862b9323a
pradyun <pradyunsg@gmail.com>
2013-10-26 15:07:39 +0530

    Just a few updates to the code..
    AST parser now takes a parent class as argument. Update README.md.

--- c_ast.py
+++ c_ast.py
@@ -36,8 +36,10 @@ class Parser(object):
     """Loads the AST dynamically into classes
 
     This Parser parses the file passed in through """
-    def __init__(self):
+    def __init__(self, parent_class):
         super(Parser, self).__init__()
+        self._parent = parent_class
+
         self.data = []
         self.func_string = """
         def __init__(self{formed_args}):
@@ -55,14 +57,17 @@ class Parser(object):
             msg = ("Expected file(-like) object with 'read' method, not found "
                    "in object of type {0!r} object".format(type(f)))
             raise TypeError(msg)
+
         # have got the text by now
         def split_args(li):
             "Splits [object, *attrs] into [object, attrs]"
-            args = [map(lambda a: a.strip(), arg.split(','))
-                       for i, arg in enumerate(li[1:])]
+            args = [
+                map(lambda a: a.strip(), arg.split(','))
+                for i, arg in enumerate(li[1:])
+            ]
             return [li[0]] + (args if args and all(*args) else [[]])
 
-        text = re.sub(r"#.*(\n|$)", "", text) # remove comments
+        text = re.sub(r"#.*(\n|$)", "", text)  # remove comments
         lines = filter(lambda x: len(x) > 1, text.splitlines())
         data = map(lambda x: x.split(':'), lines)
         self.data = map(split_args, data)
@@ -76,32 +81,50 @@ class Parser(object):
         else:
             di = module
         for k, v in self.data:
-            cls = self.create_class(k, v, verbose)
+            cls, s = self.create_class(k, v, verbose)
             di[k] = cls
 
     def create_class(self, name, args, verbose):
+        s = "class {0}({1}):\n".format(name, self._parent.__name__)
+        init_string = self.create_init(args, verbose)
+        s += indent(init_string)+'\n'
         if verbose:
-            print "class {0}(AST):".format(name)
-        __init__ = self.create_init(args, verbose)
-        # make something cleanly!!
-        cls = type(name, (AST,), {'__init__': __init__})
-        return cls
+            print s
+        di = {self._parent.__name__: self._parent}
+        exec s in di
+        return di[name], s
 
     def create_init(self, args, verbose):
-        "Creates the __init__ function from arguments and returns it"
+        "Creates the __init__ function from arguments and returns it"`
         cleaned_args = '['+(', '.join(repr(x.split('=')[0]) for x in args))+']'
-        formed_args = ''.join((', '+x) for x in args)
-        setters = indent('\n'.join("self.{0} = {0}".format(i.split('=')[0])
-                                                           for i in args))
-        di = {}
+        formed_args, setters = self.form_args(args)
+        setters += indent('\n'.join("self.{0} = {0}".format(i.split('=')[0])
+                          for i in args))
         string = self.func_string.format(**locals())
-        if verbose:
-            print indent(string)+'\n\n'
-        exec self.func_string.format(**locals()) in di
-        return di['__init__']
+        return self.func_string.format(**locals())
+
+    def form_args(self, args):
+        formed = []
+        setters = []
+        for arg in args:
+            parts = arg.split('=')
+            if len(parts) > 1 and (parts[1].strip()[0], parts[1].strip()[-1]) == ('[', ']'): # noqa
+                varname = parts[0]
+                formed.append("{0}=None".format(varname))
+                s = "if {0} is None:\n    {0} = {1}\n".format(*parts)
+                setters.append(indent(s, ' '*4)+'\n')
+            else:
+                formed.append(arg)
+        if formed:
+            formed = ['']+formed
+        return ', '.join(formed), ''.join(setters)
 
 # Export Nodes
 def prepare(module=None, verbose=False):
-    p = Parser()
+    p = Parser(AST)
     p.prepare(open(os.path.join(os.path.dirname(__file__), "astnodes.txt")))
     p.setup_module(module, verbose)
+
+if __name__ == '__main__':
+    prepare()
+
