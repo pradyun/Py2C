b09fa0985c4e3086fd773fc902405cabef57822e
Pradyun <pradyunsg@gmail.com>
2013-11-17 19:56:38 +0530

    Major changes to ast node generation
     - Now all attributes need to be provided to initialize the AST nodes
     - There is no option `is_list`. All nodes can have any value (This option made no difference to anything)
     - Added some support for strings. (Only double quoted strings allowed, with no escaping for the quote)
    
    I found that controlling the behaviour of an object from it's super-class was pretty difficult. So, let Python do the init!

--- tests/test_dual_ast.py
+++ tests/test_dual_ast.py
@@ -1,54 +1,45 @@
 #!/usr/bin/env python
 
 import unittest
-
 import py2c.dual_ast as dual_ast
 
-
 class DummyNode(dual_ast.AST):
-    def __init__(self, attrs=None, *args, **kwargs):
-        if attrs is None:
-            attrs = []
-        self._attrs = attrs
-        super(DummyNode, self).__init__(*args, **kwargs)
-
+    def __init__(self, **kwargs):
+        self._fields = list(kwargs)
+        self.__dict__.update(kwargs)
 
 # -----------------------------------
 class ASTTestCase(unittest.TestCase):
     """Tests for AST node"""
-
-    def test_init1(self):
-        "A no-subclass directly invocation of init method of AST"
-        with self.assertRaises(AttributeError):
-            dual_ast.AST()
-
     def test_equality_equal1(self):
         "Test for equality for really equal Nodes"
-        attrs = [("foo", None, False), ('bar', '[]', True)]
+        attrs = [("foo", None), ('bar', '[]')]
 
-        node1 = DummyNode(attrs)
-        node2 = DummyNode(attrs)
+        node1 = DummyNode(id="1")
+        node2 = DummyNode(id="1")
 
         self.assertEqual(node1, node2)
 
     def test_equality_equal2(self):
         "Test for equality with node when it has children nodes"
-        node1 = DummyNode(dest=DummyNode(id='foo'), values=[],
-                          sep=' ', end='\n')
-        node2 = DummyNode(dest=DummyNode(id='foo'), values=[],
-                          sep=' ', end='\n')
+        node1 = DummyNode(
+            file=DummyNode(id='foo'),
+        )
+        node2 = DummyNode(
+            file=DummyNode(id='foo'),
+        )
         self.assertEqual(node1, node2)
 
     def test_equality_with_extra_attrs(self):
         "Test for (in)equality on nodes with in-equal attributes"
-        node1 = DummyNode([("foo", None, False)])
-        node2 = DummyNode([("foo", None, False), ("bar", None, False)])
+        node1 = DummyNode(a="b")
+        node2 = DummyNode(a="c")
 
         self.assertNotEqual(node1, node2)
 
     def test_equality_diff_type(self):
         "Test for inequality on the basis of type"
-        node1 = DummyNode([])
+        node1 = DummyNode(a="foo")
 
         self.assertNotEqual(node1, "I'm not equal to a Node")
 
