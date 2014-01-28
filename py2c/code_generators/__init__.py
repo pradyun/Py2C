2bd7e46c0658ba599ac34ab409854597ef0176f4
Pradyun <pradyunsg@gmail.com>
2014-01-28 11:30:38 +0530

    [Redesign] Added the concept of "Code Generator"
    Notes:
          - Needs tests, Pradyun is on it!
          - Needs to be implemented
    
    Translators shall now only exist for languages we intend to convert to
    the AST, i.e. only Python (currently).
    A Code Generator
       - converts the AST into source code.
       - lives as a module or package within "py2c.code_generators"
       - Inherits from "py2c.base_codegenerator.AbstractCodeGenerator"
       - More details to be discussed and decided.

---
+++ py2c/code_generators/__init__.py
@@ -0,0 +1,2 @@
+"""This package holds the CodeGenerators used to convert the Py2C AST into code
+"""
--- py2c/dual_ast.py
+++ py2c/dual_ast.py
@@ -8,28 +8,30 @@ the specification in '_ast_nodes.cfg' and then imports it.
 This way, we guarantee that even if we don't ship the '_dual_ast.py' file,
 we generate it for the user on first run.
 
-Note: For development, just change the ``BUILD`` variable to ``True``.
+Note: During testing, just change the ``DEV`` variable to ``True``.
       Then, the module will be rebuilt (and replaced) even if it exists.
 """
+#-------------------------------------------------------------------------------
+# Building the module
+#-------------------------------------------------------------------------------
+from os.path import join, realpath, dirname, exists
 from . import _ast_gen as ast_gen
 
-from os.path import join, realpath, dirname, exists
-
 FNAME = join(dirname(realpath(__file__)), "_dual_ast.py")
-# If true, the module will be rebuilt (and replaced) even if it exists.
-BUILD = True
+del join, realpath, dirname
 
-def generate_module():
+DEV = True  # Determines if the module should be (re-)generated
+
+# Build if in development mode or if the file doesn't exist
+if DEV or not exists(FNAME):
     with open(FNAME, "w") as f:
         f.truncate()
         ast_gen.generate(f)
 
-# Build if the file doesn't exist
-if not BUILD and not exists(FNAME):
-    BUILD = True
-if BUILD:
-    generate_module()
 
+#-------------------------------------------------------------------------------
+# Exporting Names
+#-------------------------------------------------------------------------------
 # The module exists, import it
 from . import _dual_ast
 
@@ -38,10 +40,10 @@ __all__ = filter(lambda x: not x.startswith("_"), dir(_dual_ast))
 
 # Remove all the declared names from namespace before import!
 del (
-    BUILD, FNAME, join, realpath, dirname, exists, generate_module,
+    DEV, FNAME, exists,
     # Imported modules
     _dual_ast, ast_gen
 )
 
-# The file should exist by now. Import the stuff!
+# The file will exist by now. Import the stuff!
 from ._dual_ast import *
