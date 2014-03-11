af19e4ff2c4004035858deacb4689ad13646dd34
Pradyun <pradyunsg@gmail.com>
2014-03-11 14:53:05 +0530

    [API, TESTS] Changed syntax of AST nodes declaration
    Changes:
     - Cleared '_ast_gen.py' for re-implementing.
     - Rewrote the AST declarations on the basis of Python's AST's ASDL
       definition.
     - Changed naming scheme for declaration files. They now use a ".ast"
       extension, preceded by the name of the language(s) they are for.
     - Changed API of '_ast_gen.py'.
     - Modified 'dual_ast.py' to assist the changes in following commit.
     - All tests for '_ast_gen.py' have been re-written accordingly.
    
    TODO:
     - Write tests for 'dual_ast.py'
     - Implement '_ast_gen.py' completely.

--- tests/test_dual_ast.py
+++ tests/test_dual_ast.py
@@ -6,135 +6,24 @@ import unittest
 import py2c.dual_ast as dual_ast
 
 
-class DummyNode(dual_ast.AST):
-    def __init__(self, **kwargs):
-        self._fields = list(kwargs)
-        self.__dict__.update(kwargs)
-
-
-# -----------------------------------
 class ASTTestCase(unittest.TestCase):
-    """Tests for AST node"""
-    def test_equality_equal1(self):
-        "Test for equality for really equal Nodes"
+    """Tests for base tree node class: AST
+    """
+    def test_equality_1(self):
+        """Test for equality for in really equal Nodes
+        """
 
-        node1 = DummyNode(id="1")
-        node2 = DummyNode(id="1")
+    def test_equality_2(self):
+        """Test for equality with node when it has children nodes
+        """
 
-        self.assertEqual(node1, node2)
-
-    def test_equality_equal2(self):
-        "Test for equality with node when it has children nodes"
-        node1 = DummyNode(
-            file=DummyNode(id='foo'),
-        )
-        node2 = DummyNode(
-            file=DummyNode(id='foo'),
-        )
-        self.assertEqual(node1, node2)
-
-    def test_equality_with_extra_attrs(self):
-        "Test for (in)equality on nodes with in-equal attributes"
-        node1 = DummyNode(a="b")
-        node2 = DummyNode(a="c")
-
-        self.assertNotEqual(node1, node2)
+    def test_inequality_with_diff_value(self):
+        """Test for inequality on nodes with in-equal attributes
+        """
 
     def test_equality_diff_type(self):
-        "Test for inequality on the basis of type"
-        node1 = DummyNode(a="foo")
-
-        self.assertNotEqual(node1, "I'm not equal to a Node")
-
-    # A really important and useful test. Create the node_names list!!
-    def test_nodes_exist(self):  # noqa
-        "Checks if to see that all required nodes exist in '_dual_ast.py'"
-
-        node_names = [
-            # The all important node
-            "AST", "iter_fields",
-            ## Base nodes
-            "boolop", "cmpop", "expr", "expr_context", "mod", "operator",
-            "slice", "stmt", "unaryop",
-            ## Common Nodes
-            "Module",  # Holds all code
-
-            "BinOp",  # comparision
-                "Add", "Div", "FloorDiv", "LShift", "Mod", "Mult", "Pow",  # noqa
-                "RShift", "Sub",
-                "BitAnd", "BitOr", "BitXor",
-            "BoolOp",
-                "And", "Or",
-            "UnaryOp",
-                "UAdd", "USub", "Not", "Invert",
-            # flow control/logic
-            "Break", "If", "FunctionDef", "Return", "While",
-            # Others
-            "Str",
-            ## Python nodes
-            # Comparision
-            "Is", "IsNot", "In", "NotIn",
-            # Simple statements
-            "Assert", "Assign", "AugAssign", "Pass", "Del", "Print", "Yield",
-            "Raise", "Import", "ImportFrom", "Global", "Exec",
-            "Attribute",
-            "AugLoad", "AugStore", "Bytes", "Call",
-            "ClassDef", "Compare", "Continue", "Delete", "Dict",
-            "DictComp", "Ellipsis", "Eq", "ExceptHandler",
-            "Expr", "ExtSlice",
-            "GeneratorExp", "Gt", "GtE", "IfExp", "Index",
-            "Lambda", "List", "ListComp", "Load", "Lt", "LtE",
-            "Name", "Nonlocal", "NotEq", "Num", "Param",
-            "Py_For", "Repr", "Set",
-            "SetComp", "Slice", "Starred", "Store", "Subscript", "Try",
-            "TryExcept", "TryFinally", "Tuple", "With",
-            "YieldFrom",
-            # Helper Nodes
-            "alias", "arg", "arguments", "comprehension", "excepthandler",
-            "keyword", "withitem",
-            ## C nodes
-            "Block", "C_For", "Case", "Char", "Decl", "Default", "DoWhile",
-            "EnumDecl", "EnumItem", "Float", "Goto", "Int", "Label", "Switch",
-        ]
-        # Make sure there are no duplicate elements
-        self.assertSequenceEqual(
-            sorted(list(set(node_names))),
-            sorted(node_names)
-        )
-
-        missing = []
-        for name in node_names:
-            if not hasattr(dual_ast, name):
-                missing.append(name)
-
-        extra = []
-        for name in dir(dual_ast):
-            if name not in node_names and not name.startswith("_"):
-                extra.append(name)
-
-        # If we are here, we have failed the test
-        msg = ["dual_ast has "]
-        write = msg.append
-
-        if missing and extra:
-            write("missing and extra")
-        elif missing:
-            write("missing")
-        elif extra:
-            write("extra")
-        else:
-            return
-
-        write(" attributes")
-
-        if missing:
-            write("\nMissing: ")
-            write(", ".join(missing))
-        if extra:
-            write("\nExtra: ")
-            write(", ".join(extra))
-
-        self.fail("".join(msg))
+        """Test for inequality on the basis of type
+        """
 
 
 if __name__ == '__main__':
