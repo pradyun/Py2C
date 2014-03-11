4e7d5384f122f45679c3d27153debeaf0fd352da
Pradyun <pradyunsg@gmail.com>
2014-03-11 21:39:21 +0530

    [TESTS] Switched to nose for running tests
    All tests are now run by nose. Coverage is checked with the plugin for
    nose. setup.py script now uses nose to run the tests too.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,1 @@
-# Imported, has to be package import
-if __name__ != '__main__':
-    from . import runner
-    tests = runner.load()
-else:
-    import os
-    import sys
-    sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-    import runner
-    runner.main(buffer=True)
+# Empty
