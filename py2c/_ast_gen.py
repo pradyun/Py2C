c40fd1fc117c4b4c2a53059387d3a33eecac61a3
Pradyun <pradyunsg@gmail.com>
2014-03-12 12:18:06 +0530

    [LEGAL] Added DISCLAIMER to all source files.
    All source files now have the GPL v3 Header.
    Shebang now refers to 'usr/bin/python'

--- py2c/_ast_gen.py
+++ py2c/_ast_gen.py
@@ -1,13 +1,27 @@
-#! /usr/bin/env python
+#!/usr/bin/python
 """AST nodes file generator
 
 This file generates the ast that we use as the meduim to translate Python code.
 It makes it possible to translate Python code to C code without multiple AST
 systems.
 """
+
+# Py2C - A Python to C++ compiler
+# Copyright (C) 2014 Pradyun S. Gedam
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import re
 from textwrap import dedent
--- tests/test_dual_ast.py
+++ tests/test_dual_ast.py
@@ -1,6 +1,22 @@
-#!/usr/bin/env python
-"""Implemetation Tests for the AST system used
+#!/usr/bin/python
+"""Tests for the AST used to represent Python and C in one Tree.
 """
+
+# Py2C - A Python to C++ compiler
+# Copyright (C) 2014 Pradyun S. Gedam
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import py2c.dual_ast as dual_ast
