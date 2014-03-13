b73b49919dfa90f9a904c181b792dfe92e87b50f
Pradyun <pradyunsg@gmail.com>
2014-03-13 15:14:57 +0530

    [MISC] Added Disclaimer Borders, Python 3 shebang
    Added border to disclaimer on top of files. Cleaned and added disclaimer
    to setup.py.
    Changed all shebangs to Python 3.

--- py2c/dual_ast.py
+++ py2c/dual_ast.py
@@ -1,30 +1,25 @@
-#!/usr/bin/python
-"""The module serves as a wrapper to the generated file (_dual_ast.py).
-If '_dual_ast.py' doesn't exist, it gets '_ast_gen.py' to generate it from
-the specification in '_ast_nodes.cfg' and then imports it.
-
-This way, we guarantee that even if we don't ship the '_dual_ast.py' file,
-we generate it for the user on first run.
-
-Note: During testing, just change the ``DEV`` variable to ``True``.
-      Then, the module will be rebuilt (and replaced) even if it exists.
+#!/usr/bin/python3
+"""Provides the AST system according to the declaration files
 """
 
+#-------------------------------------------------------------------------------
 # Py2C - A Python to C++ compiler
 # Copyright (C) 2014 Pradyun S. Gedam
-
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
 
 #-------------------------------------------------------------------------------
 # Building the module
