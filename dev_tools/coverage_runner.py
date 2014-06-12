836459b3e1896180b242ca70542545f92a8de264
Pradyun <pradyunsg@gmail.com>
2014-06-13 03:49:24 +0530

    [TRAVIS] Try to get ast_gen into coverage report.

--- dev_tools/coverage_runner.py
+++ dev_tools/coverage_runner.py
@@ -13,14 +13,15 @@ base_dir = realpath(join(__file__, ".."))
 cov = coverage.coverage(config_file=join(base_dir, ".coveragerc"))
 
 cov.start()
-success = nose.run(defaultTest=join(base_dir, "..", "py2c"))
+success = nose.run(defaultTest=join(base_dir, ".."))
 cov.stop()
 cov.save()
+
 if success:
     # If we are in CI environment, don't write an HTML report.
     if os.environ.get("CI", None) is None:
         cov.html_report()
-
+    # Just write the report anyway.
     cov.report()
 
 sys.exit(0 if success else 1)
