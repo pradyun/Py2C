2ab8cc06dd297c4a8e00ba0f45de971132432331
Pradyun <pradyunsg@gmail.com>
2014-06-12 20:21:10 +0530

    [TRAVIS] Fix coverage_runner for Travis.
    coverage_runner should now:
      - Return the exit code of the test run
      - Report only when tests pass

--- dev_tools/coverage_runner.py
+++ dev_tools/coverage_runner.py
@@ -2,6 +2,7 @@
 """
 
 import os
+import sys
 from os.path import join, realpath
 
 # Third Party modules
@@ -11,12 +12,15 @@ import coverage
 cov = coverage.coverage(branch=True)
 
 cov.start()
-nose.run(defaultTest=realpath(join(__file__, "..", "..", "py2c")))
+result = nose.run(defaultTest=realpath(join(__file__, "..", "..", "py2c")))
 cov.stop()
 cov.save()
 
-# If we are in CI environment, don't write an HTML report.
-if os.environ.get("CI", None) is None:
-    cov.html_report()
+if result == 0:
+    # If we are in CI environment, don't write an HTML report.
+    if os.environ.get("CI", None) is None:
+        cov.html_report()
 
-cov.report()
+    cov.report()
+
+sys.exit(result)
