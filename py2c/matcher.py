0a7da286db02183dbd204c0bcbd47a73997c0b63
Pradyun <pradyunsg@gmail.com>
2014-09-22 20:30:16 +0530

    [TESTS, MATCHER] Complete the implementation of Matchers and related tests..
    Also, not print of firefox's errors in output stream...

--- py2c/matcher.py
+++ py2c/matcher.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""Contains matchers for the modifiers
+"""Implements matchers for the modifiers
 """
 import abc
 
@@ -8,12 +6,12 @@ import abc
 class Matcher(object, metaclass=abc.ABCMeta):
     """Base class of all matchers
     """
+
     def __init__(self):
         super().__init__()
-        self._name_stack = []
 
     @abc.abstractmethod  # coverage: no partial
-    def matches(self, node):
+    def match(self, node):
         """Match against the node
 
         Arguments
@@ -24,9 +22,13 @@ class Matcher(object, metaclass=abc.ABCMeta):
         Returns
         -------
             A bool, denoting if the node is a match.
-        """
 
-    def match(self, matcher, value):
+        Matching is based solely on this attribute of the Matcher object.
+        This object defines what matches the nodes.
+        """
+        raise NotImplementedError()
+
+    def generic_match(self, matcher, value):
         """Match the value to matcher
 
         Arguments
@@ -44,26 +46,45 @@ class Matcher(object, metaclass=abc.ABCMeta):
         if isinstance(matcher, built_in_types):
             return matcher == value
         elif isinstance(matcher, Matcher):
-            return matcher.matches(value)
+            return matcher.match(value)
         else:
             print("Unknown matcher: {}".format(matcher))
             return False
 
 
 class Instance(Matcher):
-    """Matches an instance of a class
+    """Matches if object isinstance of the class passed initially.
     """
-    def __init__(self, cls, attrs={}):
+
+    def __init__(self, cls, attrs=None):
         super().__init__()
         self._cls = cls
-        self._attrs = attrs
+        if attrs is not None:
+            self._attrs = Attributes(attrs)
+        else:
+            self._attrs = None
 
-    def matches(self, node):
+    def match(self, node):
         if not isinstance(node, self._cls):
             return False
+        elif self._attrs is not None:
+            return self._attrs.match(node)
+        else:
+            return True
+
+
+class Attributes(Matcher):
+    """Matches if attributes of an object are same as those passed during \
+    initalization of an instance of this class.
+    """
+
+    def __init__(self, attrs):
+        self._attrs = attrs
+
+    def match(self, node):
         placeholder = object()
         for name, value in self._attrs.items():
             attr = getattr(node, name, placeholder)
-            if attr == placeholder or not self.match(value, attr):
+            if attr is placeholder or not self.generic_match(value, attr):
                 return False
         return True
