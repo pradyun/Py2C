e132e20f7b86686f7c298f4fba514783e71645a9
Pradyun <pradyunsg@gmail.com>
2014-09-23 16:00:57 +0530

    [MODIFIER] Implement ABC for modifiers with tests.

---
+++ py2c/modifier.py
@@ -0,0 +1,39 @@
+"""Implements an Abstract Base Class for Modifiers
+"""
+import abc
+from . import matcher
+
+
+class Modifier(object, metaclass=abc.ABCMeta):
+    """Base class of all modifier
+    """
+
+    def __init__(self):
+        super().__init__()
+        if not hasattr(self, "matcher"):
+            raise AttributeError(
+                "{} should have a matcher attribute.".format(
+                    self.__class__.__qualname__
+                )
+            )
+        elif not isinstance(self.matcher, matcher.Matcher):
+            raise TypeError(
+                "{}.matcher should be an instance of a subclass of {}".format(
+                    self.__class__.__qualname__,
+                    matcher.Matcher.__class__.__qualname__
+                )
+            )
+
+    @abc.abstractmethod  # coverage: no partial
+    def modify(self, node):
+        """Modify the tree node
+
+        Arguments:
+            node
+                A node that the Matcher matched sucessfully with.
+
+        Returns:
+            A replacement value (node), that is placed in-place of the node
+            passed.
+        """
+        raise NotImplementedError()
