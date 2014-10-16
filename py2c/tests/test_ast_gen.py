d7fa8a2cc4d4e63f4f58e2fbf497a2dba7cb4bf3
Pradyun <pradyunsg@gmail.com>
2014-10-16 16:13:17 +0530

    [MISC] Add a (psuedo?) argument to cleanup.py and change import-behavior of test_ast_gen.py

--- py2c/tests/test_ast_gen.py
+++ py2c/tests/test_ast_gen.py
@@ -12,13 +12,29 @@ from textwrap import dedent
 from py2c.tests import Test
 from nose.tools import eq_, assert_in, assert_raises
 
-# XXX: Dirty!!!
-import sys
-sys.path.append(__file__ + "/../../..")
-import ast_gen
-sys.path.pop()
-del sys
 
+def get_ast_gen():
+    # Imports ast_gen.py for testing purposes.
+    import os
+    import sys
+
+    path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    # This if statement exists to prevent the user from getting a wierd
+    # import error about some non-existent file. Instead
+    if os.path.exists(os.path.join(path, "ast_gen.py")):
+        sys.path.append(path)
+        import ast_gen
+        sys.path.pop()
+    else:
+        raise ImportError(
+            "Can't import ast_gen.py, these tests can only be run on source "
+            "package, not distribution."
+        )
+
+    return ast_gen
+
+ast_gen = get_ast_gen()
+del get_ast_gen
 
 #------------------------------------------------------------------------------
 # Tests
