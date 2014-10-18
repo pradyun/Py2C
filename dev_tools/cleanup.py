a1d8d9b049be948eedbbf9f1e4c53736e1bbc678
Pradyun <pradyunsg@gmail.com>
2014-10-18 17:29:17 +0530

    [DEV_TOOLS] Clean coverage report, test-runner. Add a new ast-printer for assisting with modifiers.

--- dev_tools/cleanup.py
+++ dev_tools/cleanup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Delete all unnecessary files and directories in repository
 """
 
--- dev_tools/run_tests.py
+++ dev_tools/run_tests.py
@@ -7,14 +7,12 @@
 # Copyright (C) 2014 Pradyun S. Gedam
 #------------------------------------------------------------------------------
 
-import os
 import sys
 from os.path import join, realpath, dirname
 
 # Third Party modules
 import nose
 import coverage
-
 
 base_dir = realpath(dirname(__file__))
 root_dir = join(dirname(base_dir), "py2c")
@@ -30,10 +28,7 @@ cov.stop()
 cov.save()
 
 if success:
-    # If we are in CI environment, don't write an HTML report.
-    if os.environ.get("CI", None) is None:
-        cov.html_report()
-    print()
+    cov.html_report()
     cov.report()
 
 sys.exit(0 if success else 1)
