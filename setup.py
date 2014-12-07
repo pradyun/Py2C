313f7cb7c716902f7f60e1df83b4541d75a8fd36
Pradyun <pradyunsg@gmail.com>
2014-12-07 11:31:48 +0530

    [SETUP] Add a bunch of things I missed...

--- setup.py
+++ setup.py
@@ -6,30 +6,36 @@
 
 # pylint:disable=C0103
 import sys
+if sys.version_info[:2] < (3, 3):
+    print("Cannot run on Python versions before Python 3.3")
+    sys.exit(1)
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
     print("Please install 'setuptools' to run this script.")
     sys.exit(1)
 
-try:
-    from setuptools.command.build_py import build_py as _build_py
-except ImportError:
-    from distutils.command.build_py import build_py as _build_py
-
 #------------------------------------------------------------------------------
 # Generating the AST
 #------------------------------------------------------------------------------
 from os.path import join, dirname, realpath
 
+try:  # If ever setuptools improves on the build_py command.
+    from setuptools.command.build_py import build_py as _build_py
+except ImportError:
+    from distutils.command.build_py import build_py as _build_py
+
 sys.path.append(realpath(dirname(__file__)))
+
 try:
     import ast_gen
-except Exception:
-    print("ERROR: Unable to generate required files...")
+except ImportError as err:
+    print("ERROR: ", err)
     print(" ----> Try again after installing PLY.")
     sys.exit(1)
-sys.path.pop()
+finally:
+    sys.path.pop()
 
 path_to_ast_definitions = realpath(join(dirname(__file__), "py2c", "ast"))
 
