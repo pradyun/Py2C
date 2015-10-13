9bfa416f32de11c560e7d61d4c11784e2c8c4471
Pradyun S. Gedam <pradyunsg@gmail.com>
2015-10-13 20:04:52 +0530

    [TREE-NODE-GEN] Port to new data-driven-test

--- py2c/tree/tests/test_node_gen.py
+++ py2c/tree/tests/test_node_gen.py
@@ -5,52 +5,54 @@ from textwrap import dedent
 
 from py2c.tree import node_gen
 
-from py2c.tests import Test, data_driven_test    # noqa
+from py2c.tests import Test, data_driven_test
 from nose.tools import assert_equal, assert_raises
-
-import py2c.tree.tests.data_node_gen as data
 
 
 # -----------------------------------------------------------------------------
 # Tests
 # -----------------------------------------------------------------------------
-@data_driven_test(data.remove_comments_cases, True, "removes comments correctly: ")  # noqa
-def test_remove_comments(test_string, expected):
+@data_driven_test("node_gen-remove_comments.yaml")
+def test_remove_comments(in_text, out_text):
     "py2c.tree.node_gen.remove_comments"
-    assert_equal(node_gen.remove_comments(test_string), expected)
+    assert_equal(node_gen.remove_comments(in_text), out_text)
 
 
 class TestParser(Test):
     """py2c.tree.node_gen.Parser
     """
 
-    @data_driven_test(data.Parser_valid_cases, True, "parses valid input correctly: ")  # noqa
-    def test_valid_cases(self, test_string, expected, _):
+    @data_driven_test("node_gen-valid_cases.yaml")
+    def test_valid_cases(self, in_text, node, **kwargs):
+        node = self.load(node, {"Definition": node_gen.Definition})
+
         parser = node_gen.Parser()
 
         assert_equal(
-            parser.parse(dedent(test_string)),
-            tuple(expected)
+            parser.parse(dedent(in_text)),
+            tuple(node)
         )
 
-    @data_driven_test(data.Parser_invalid_cases, True, "raises error parsing: ")  # noqa
-    def test_invalid_cases(self, test_string, required_phrases):
+    @data_driven_test("node_gen-invalid_cases.yaml")
+    def test_invalid_cases(self, in_text, phrases):
         with assert_raises(node_gen.ParserError) as context:
-            node_gen.Parser().parse(test_string)
+            node_gen.Parser().parse(in_text)
 
-        self.assert_error_message_contains(context.exception, required_phrases)
+        self.assert_error_message_contains(context.exception, phrases)
 
 
 class TestSourceGenerator(Test):
     """py2c.tree.node_gen.SourceGenerator
     """
 
-    @data_driven_test(data.SourceGenerator_valid_cases, True, "generates code correctly: ")  # noqa
-    def test_valid_cases(self, _, definitions, expected):
-        src_gen = node_gen.SourceGenerator()
-        generated = src_gen.generate_sources(definitions)
+    @data_driven_test("node_gen-valid_cases.yaml")
+    def test_valid_cases(self, out_text, node, **kwargs):
+        node = self.load(node, {"Definition": node_gen.Definition})
 
-        assert_equal(dedent(expected).strip(), generated.strip())
+        src_gen = node_gen.SourceGenerator()
+        generated = src_gen.generate_sources(node)
+
+        assert_equal(out_text.strip(), generated.strip())
 
 if __name__ == '__main__':
     from py2c.tests import runmodule
